f57de9490399bd623f996006e561ae6272b9ada5
Marvin Song <weitao.song@dorabot.com>
2019-08-19 14:57:38 +0800

    cmake on linux

--- src/main.cc
+++ src/main.cc
@@ -4,12 +4,12 @@
 #include "transceiver.h"
 
 int main(int, char**){
-	// HostGui host;
-	// std::shared_ptr<HostGui> host_ptr;
-	auto cpn_ptr = std::make_shared<Component>();
-	Transceiver tc;
-	tc.AddSubscriber({MsgType::kOut, "msgtest"}, cpn_ptr);
-	while(tc.is_on());
+	HostGui host;
+	std::shared_ptr<HostGui> host_ptr;
+	// auto cpn_ptr = std::make_shared<Component>();
+	// Transceiver tc;
+	// tc.AddSubscriber({MsgType::kOut, "msgtest"}, cpn_ptr);
+	// while(tc.is_on());
 
 	std::cout << "~done~\n";
 	return 0;
